ec453c7b63241270167bc6dbcc80df4ba701ce44
Bruno Cruz <152812323+BC-Estrategias@users.noreply.github.com>
2026-09-20 22:48:50 -0300

    fix: share neutral 3DS mapping declarations

--- port/port_runtime_config.h
+++ port/port_runtime_config.h
@@ -11,29 +11,7 @@
 extern "C" {
 #endif
 
-typedef enum {
-    PORT_3DS_MAP_X = 0,
-    PORT_3DS_MAP_Y,
-    PORT_3DS_MAP_ZL,
-    PORT_3DS_MAP_ZR,
-    PORT_3DS_MAP_CSTICK,
-    PORT_3DS_MAP_COUNT
-} Port3DSMapButton;
-
-typedef enum {
-    PORT_3DS_ACTION_NONE = 0,
-    PORT_3DS_ACTION_TABS,
-    PORT_3DS_ACTION_TURBO,
-    PORT_3DS_ACTION_ITEM,
-    PORT_3DS_ACTION_SAVE_STATE,
-    PORT_3DS_ACTION_LOAD_STATE,
-    PORT_3DS_ACTION_COUNT
-} Port3DSMappedAction;
-
-int Port_Config_Get3DSButtonAction(int button);
-void Port_Config_Set3DSButtonAction(int button, int action);
-void Port_Config_Cycle3DSButtonAction(int button);
-const char* Port_Config_Get3DSButtonActionName(int action);
+#include "port_button_mapping.h"
 
 typedef enum {
     PORT_INPUT_A,
